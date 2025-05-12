d2238af931d791bc4922c16928cecf05429a6c49
b0ngCloud <87395488+Bizzerowasnotavailable@users.noreply.github.com>
2025-05-12 09:44:25 +0200

    first commit (prolly last idk)

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	using namespace std;
+	string a;
+	a = "explosion prevention";
+	cout << "bunnysay" << endl << "like cowsay, but way worse" << endl;
+	cout << "tell me: ";
+	getline (cin, a);
+cout<< "|￣￣￣￣￣￣￣|" << endl << a << endl <<
+	"|＿＿＿＿＿＿＿|" << endl << 
+	"       ||" << endl <<
+	"(•ㅅ•) ||" << endl <<
+	"/ 　 づ" << endl << "" << endl << "by Bizzero - 2025" << endl ;
+	
+    return 0;
+}
